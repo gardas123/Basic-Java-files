ce1a77bc55d50cce0b0b6c1a7d3653bee36df976
gardas123 <prashaantgardas64@gmail.com>
2025-02-12 20:22:10 +0530

    Static block

---
+++ StaticBlock.java
@@ -0,0 +1,8 @@
+class StaticBlock{
+	static{
+		System.out.println("This is inside of Static method"); // Static Block
+	}
+	public static void main(String arg[]){
+		System.out.println("This is inside of main");
+	}
+}
